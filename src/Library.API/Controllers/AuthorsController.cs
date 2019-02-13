64aa5327743ab46aa6cc7efa256d2f6e04ea8ce8
Ece Ercan <ece.ercan@ericsson.com>
2019-02-13 15:11:00 +0300

    AuthorsController is added

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,29 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        // yukarıya route ekleyerek bütün http methodlarının api/authors ile başlayacağını söylemiş olduk. HttpGet'e de birşey yazmayarak 
+        // direk route'tan gelen api/authors ile erişilebileceğini öylemiş olduk
+        [HttpGet()] 
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
